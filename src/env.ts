7e63a2a18eb791ba974457328b8033f94b793db3
isel-jao <iseljao89@gmail.com>
2023-11-21 18:07:51 +0100

    init

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,10 @@ const envSchema = z.object({
   BROKER_URL: z.string().url().default("mqtt://localhost:1883"),
   BROKER_USERNAME: z.string().default("isel"),
   BROKER_PASSWORD: z.string().default("isel123"),
+  DEBUG: z
+    .string()
+    .transform((val) => val === "true")
+    .default("true"),
 });
 
 export type Env = z.infer<typeof envSchema>;
--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,74 @@ const client = mqtt.connect(env.BROKER_URL, {
   password: env.BROKER_PASSWORD,
 });
 
-const topic = "data";
+const serials = ["A123", "B456", "C789"];
+
+const fakeOperation = async (serial: string) => {
+  const topic = `devices/${serial}/events`;
+  while (true) {
+    const operationTime = Math.floor(Math.random() * 30);
+    await new Promise((resolve) => setTimeout(resolve, operationTime * 1000));
+    const operationSuccess = Math.random() > 0.2;
+    const message = operationSuccess
+      ? "OPERATION DONE"
+      : "ERROR: something went wrong";
+
+    client.publish(topic, message, (err) => {
+      if (err) console.error("Error publishing message:", err);
+      else if (env.DEBUG)
+        console.log(`Message published to topic "${topic}": ${message}`);
+    });
+  }
+};
+
+const fakeEnergy = async (serial: string) => {
+  const topic = `nxt/${serial}/energy/data`;
+
+  while (true) {
+    const message = JSON.stringify({
+      id: serial,
+      dt: new Date().toISOString(),
+      voltage: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+      current: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+      power: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+      pf: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+      total: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+    });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    client.publish(topic, message, (err) => {
+      if (err) console.error("Error publishing message:", err);
+      else if (env.DEBUG)
+        console.log(`Message published to topic "${topic}": ${message}`);
+    });
+  }
+};
 
 client.on("connect", () => {
   console.log("Connected to MQTT broker");
 
-  // Publish a message to the specified topic
-  const message = "Hello, MQTT!";
-  client.publish(topic, message, (err) => {
-    if (err) {
-      console.error("Error publishing message:", err);
-    } else {
-      console.log(`Message published to topic "${topic}": ${message}`);
-      // Disconnect after publishing the message
-      client.end();
-    }
+  serials.forEach((serial) => {
+    fakeOperation(serial);
+    fakeEnergy(serial);
   });
 });
 
